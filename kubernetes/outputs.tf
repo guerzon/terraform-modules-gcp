4fecbc2c1bb9c08738c186c0c3b4c557615d3315
Lester Guerzon <guerzon@proton.me>
2025-07-13 17:51:24 +0800

    fix(kubernetes): expose cluster endpoint and cert (#3)

--- kubernetes/outputs.tf
+++ kubernetes/outputs.tf
@@ -2,3 +2,13 @@ output "cluster_id" {
   description = "The Kubernetes cluster ID."
   value       = google_container_cluster.default.id
 }
+
+output "cluster_endpoint" {
+  description = "The Kubernetes endpoint."
+  value       = google_container_cluster.default.endpoint
+}
+
+output "cluster_ca_certificate" {
+  description = "Cluster CA certificate"
+  value       = google_container_cluster.default.master_auth.0.cluster_ca_certificate
+}
