e5f9ff01ca8625d2450b53434541b5a13b2abc2e
Lester Guerzon <guerzon@proton.me>
2026-05-26 16:30:16 +0800

    feat(database): change from password_wo to password (#6)

--- database/main.tf
+++ database/main.tf
@@ -6,5 +6,5 @@ resource "google_sql_database" "default" {
 resource "google_sql_user" "default" {
   name        = var.database_user
   instance    = var.sql_instance_name
-  password_wo = var.database_password
+  password    = var.database_password
 }
